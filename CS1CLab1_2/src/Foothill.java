6c041b25477625af5040dbada8a52a0fc4326ee2
Student <Student@153.18.35.4>
2016-01-07 20:05:39 -0800

    check

--- CS1CLab1_2/src/Foothill.java
+++ CS1CLab1_2/src/Foothill.java
@@ -1,12 +1,45 @@
+import cs_1c.*;
+import java.text.*;
+import java.util.*;
 
-public class Foothill {
+//------------------------------------------------------
+public class Foothill
+{
+   // -------  main --------------
+   public static void main(String[] args) throws Exception
+   {
+      int target = 3600;
+      ArrayList<iTunesEntry> dataSet = new ArrayList<iTunesEntry>();
+      ArrayList<Sublist> choices = new ArrayList<Sublist>();
+      int k, j, numSets, max, kBest, arraySize, masterSum;
+      boolean foundPerfect;
+      
+      // for formatting and timing
+      NumberFormat tidy = NumberFormat.getInstance(Locale.US);
+      tidy.setMaximumFractionDigits(4);
+      long startTime, stopTime; 
+      
+      // read the iTunes Data
+      iTunesEntryReader tunesInput = new iTunesEntryReader("itunes_file.txt");
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+      // test the success of the read:
+      if (tunesInput.readError())
+      {
+         System.out.println("couldn't open " + tunesInput.getFileName()
+            + " for input.");
+         return;
+      }
 
-	}
+      // load the dataSet ArrayList with the iTunes:
+      arraySize = tunesInput.getNumTunes();
+      for (k = 0; k < arraySize; k++)
+         dataSet.add(tunesInput.getTune(k));
 
+      choices.clear();
+      System.out.println("Target time: " + target);
+      
+      // code supplied by student
+
+      choices.get(kBest).showSublist();
+   }
 }
